c5c900059e0c445e6eac5d2f2b26e2f74198317e
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2025-03-13 11:09:39 +0900

    feat: completed popup and banner pages

---
+++ banner-korean.js
@@ -0,0 +1,11 @@
+export default {
+  currentLanguage: "KO",
+  upTo50Discount: "최대 50% 할인!",
+  shopNow: "지금 쇼핑하기",
+  aWarmerYearEndThanEverSmartWaysToPrepareForTheNewYear:
+    "어느때보다 따듯한 연말, 알차게 새해 준비하는 방법 ",
+  evenGreaterBenefitsInDecember: "12월에는 더 큰 혜택으로!",
+  onlineOffline: "온&오프라인",
+  firstTiemOnlinePurchaseLimited40CouponGiveaway:
+    "온라인몰 첫구매시 선착순 40% 쿠폰 증정! ",
+};
